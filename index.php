9f3c629fac329417fb85487e01c05c24b51cfba5
MariaCelorio <54629691+MariaCelorio@users.noreply.github.com>
2019-12-05 01:40:00 -0600

    Update index.php

--- index.php
+++ index.php
@@ -223,8 +223,7 @@
         <script src="vendors/isotope/isotope-min.js"></script>
         <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
         <script src="js/jquery.ajaxchimp.min.js"></script>
-        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
-        <script src="vendors/counter-up/jquery.counterup.js"></script>
+        
         <script src="js/mail-script.js"></script>
         <script src="js/theme.js"></script>
     </body>
